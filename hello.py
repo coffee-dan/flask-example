aa443a00e308dae5e656822b55c99c4e30575068
coffee-dan <coffee-dan@protonmail.com>
2021-02-04 09:08:53 -0600

    Adding Bootstrap and Template Inheritance

--- hello.py
+++ hello.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # function decorator for defining where this function is shown
-@app.route('/<name>')
-def home(name):
-    return render_template("index.html", content=['tim', 'joe', 'bill'])
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
